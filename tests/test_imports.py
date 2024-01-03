5b0fb2710f60141952a78011a07b743a76cf35ac
Riccardo <trevisan.ricc@gmail.com>
2024-01-03 15:14:17 +0100

    Working test workflow

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,24 +1,22 @@
-# tests/test_imports.py
+# tests\test_imports.py
 import sys
 import os
 
 # Append the project root directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-#def test_import_prosumer_model():
-#    import ProsumerModel as pyp
-#    assert True  # Example assertion
-#
-#def test_import_consumer_model(): 
-#    import ConsumerModel as pyc
-#    assert True
+def test_import_prosumer_model():
+    import ProsumerModel as pyp
+    assert True  # Example assertion
 
+def test_import_consumer_model(): 
+    import ConsumerModel as pyc
+    assert True
 
 def test_other_impports():
     import numpy as np
     import pandas as pd
     from pathlib import Path
-    import pyomo
     import os
     import pprint
     import functions
