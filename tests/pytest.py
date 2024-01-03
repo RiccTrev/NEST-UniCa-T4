bc26eff224afe775a4ff982adde8aa6617f6728a
Riccardo <trevisan.ricc@gmail.com>
2024-01-03 12:49:30 +0100

    Created tests/pytest

---
+++ tests/pytest.py
@@ -0,0 +1,16 @@
+def import_test(): 
+    import ProsumerModel as pyp
+    import ConsumerModel as pyc
+    import numpy as np
+    import pandas as pd
+    from pathlib import Path
+    import os
+    #import sys
+    #sys.path.append('../')
+    import pprint
+    import functions
+    import parametri
+    import warnings
+    import pprint
+    import json
+
