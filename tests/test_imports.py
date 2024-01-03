56fbe6aa0826729fdf755663e816172238e86733
Riccardo <trevisan.ricc@gmail.com>
2024-01-03 13:28:47 +0100

    5

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,17 +1,29 @@
 # tests/test_imports.py
-def test_import(): 
+import sys
+import os
+
+# Append the project root directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def test_import_prosumer_model():
     import ProsumerModel as pyp
+    assert True  # Example assertion
+
+def test_import_consumer_model(): 
     import ConsumerModel as pyc
+    assert True
+
+
+def test_other_impports():
     import numpy as np
     import pandas as pd
     from pathlib import Path
+    import Pyomo
+    import astral
     import os
-    #import sys
-    #sys.path.append('../')
     import pprint
     import functions
     import parametri
     import warnings
-    import pprint
     import json
-
+    assert True
